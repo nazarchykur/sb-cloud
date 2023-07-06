2eebe119b9d2b76c41d2c0580296fdba7f437547
nazarchykur <nazarchykur@gmail.com>
2023-07-06 13:46:08 +0300

    rewrote some props

---
+++ config-server-github/src/test/java/com/example/configservergithub/ConfigServerGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configservergithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
